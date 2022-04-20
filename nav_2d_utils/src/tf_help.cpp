5dce233b5f984d83b23bc9183aef0edfd40bd455
David V. Lu <davidvlu@gmail.com>
2022-04-20 10:06:11 -0400

    Fix for #60

--- nav_2d_utils/src/tf_help.cpp
+++ nav_2d_utils/src/tf_help.cpp
@@ -2,6 +2,7 @@
  * Software License Agreement (BSD License)
  *
  *  Copyright (c) 2017, Locus Robotics
+ *                2022, Metro Robots
  *  All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without
@@ -32,60 +33,68 @@
  *  POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef NAV_2D_UTILS_TF_HELP_H
-#define NAV_2D_UTILS_TF_HELP_H
-
-#include <nav_core2/common.h>
-#include <geometry_msgs/PoseStamped.h>
+#include <nav_2d_utils/tf_help.h>
 #include <nav_2d_msgs/Pose2DStamped.h>
+#include <nav_2d_utils/conversions.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <string>
 
 namespace nav_2d_utils
 {
-/**
- * @brief Transform a PoseStamped from one frame to another while catching exceptions
- *
- * Also returns immediately if the frames are equal.
- * @param tf Smart pointer to TFListener
- * @param frame Frame to transform the pose into
- * @param in_pose Pose to transform
- * @param out_pose Place to store the resulting transformed pose
- * @param extrapolation_fallback If true, if there is an ExtrapolationException, allow looking up the latest timestamp instead.
- * @return True if successful transform
- */
 bool transformPose(const TFListenerPtr tf, const std::string frame,
                    const geometry_msgs::PoseStamped& in_pose, geometry_msgs::PoseStamped& out_pose,
-                   const bool extrapolation_fallback = true);
+                   const bool extrapolation_fallback)
+{
+  if (in_pose.header.frame_id == frame)
+  {
+    out_pose = in_pose;
+    return true;
+  }
 
-/**
- * @brief Transform a Pose2DStamped from one frame to another while catching exceptions
- *
- * Also returns immediately if the frames are equal.
- * @param tf Smart pointer to TFListener
- * @param frame Frame to transform the pose into
- * @param in_pose Pose to transform
- * @param out_pose Place to store the resulting transformed pose
- * @param extrapolation_fallback If true, if there is an ExtrapolationException, allow looking up the latest timestamp instead.
- * @return True if successful transform
- */
+  try
+  {
+    tf->transform(in_pose, out_pose, frame);
+    return true;
+  }
+  catch (tf::ExtrapolationException& ex)
+  {
+    if (!extrapolation_fallback)
+      throw;
+    geometry_msgs::PoseStamped latest_in_pose;
+    latest_in_pose.header.frame_id = in_pose.header.frame_id;
+    latest_in_pose.pose = in_pose.pose;
+    tf->transform(latest_in_pose, out_pose, frame);
+    return true;
+  }
+  catch (tf::TransformException& ex)
+  {
+    ROS_ERROR("Exception in transformPose: %s", ex.what());
+    return false;
+  }
+  return false;
+}
+
 bool transformPose(const TFListenerPtr tf, const std::string frame,
                    const nav_2d_msgs::Pose2DStamped& in_pose, nav_2d_msgs::Pose2DStamped& out_pose,
-                   const bool extrapolation_fallback = true);
+                   const bool extrapolation_fallback)
+{
+  geometry_msgs::PoseStamped in_3d_pose = pose2DToPoseStamped(in_pose);
+  geometry_msgs::PoseStamped out_3d_pose;
 
-/**
- * @brief Transform a Pose2DStamped into another frame
- *
- * Note that this returns a transformed pose
- * regardless of whether the transform was successfully performed.
- *
- * @param tf Smart pointer to TFListener
- * @param pose Pose to transform
- * @param frame_id Frame to transform the pose into
- * @return The resulting transformed pose
- */
+  bool ret = transformPose(tf, frame, in_3d_pose, out_3d_pose, extrapolation_fallback);
+  if (ret)
+  {
+    out_pose = poseStampedToPose2D(out_3d_pose);
+  }
+  return ret;
+}
+
 geometry_msgs::Pose2D transformStampedPose(const TFListenerPtr tf, const nav_2d_msgs::Pose2DStamped& pose,
-                                           const std::string& frame_id);
+                                           const std::string& frame_id)
+{
+  nav_2d_msgs::Pose2DStamped local_pose;
+  nav_2d_utils::transformPose(tf, frame_id, pose, local_pose);
+  return local_pose.pose;
+}
 
 }  // namespace nav_2d_utils
-
-#endif  // NAV_2D_UTILS_TF_HELP_H
